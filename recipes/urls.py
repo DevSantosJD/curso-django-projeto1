b0406b5569ca6ec7bb3ef198738f826401b337ea
Jardel Santos <jardel.santos.23@outlook.com>
2024-11-18 16:07:58 -0300

    atualização - movendo funções para as views e organizando URLs

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from recipes.views import home, contato, sobre
 
 urlpatterns = [
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+    path('', home), #/home
+    path('sobre/', sobre), #/sobre
+    path('contato/', contato), #/contato
 ] 
