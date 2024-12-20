3c6abe0ee9b7abf4628f8b89a464e99fbef3d725
DevSantosJD <jardel.santos.23@outlook.com>
2024-12-19 21:42:02 -0300

    atualização de view

--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')# teste página raiz home 
+    return render(request, 'recipes/home.html')
 
-def contato(request):
-    return HttpResponse('contato (11) 97070-7070') #test de retorno
-
-def sobre(request):
-    return HttpResponse('Desenvolvedor Back-end Python Django') #teste de retorno com um toque de humor
